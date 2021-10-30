aa8852d86cd089a3b645b80f66e3b75b3f9a4d97
tea577577 <108051189@gm.asia.edu.tw>
2021-10-30 18:12:07 +0800

    1030增加程式
    增加程式

---
+++ Assets/程式/level.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class level : MonoBehaviour
+{
+    #region
+    
+    public int[] levels = new int[100];
+
+    private void Start()
+    {
+        
+        for (int i = 0; i < 100; i++) ;
+        
+    }
+    #endregion
+}
